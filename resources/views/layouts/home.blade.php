48f0fe05f57c0a4c5eec169d1ecfa2fd73296b0b
Adza Zarif Nur iskandar <adzazarf@gmail.com>
2025-03-15 19:51:49 +0700

    fixing

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -203,7 +203,7 @@
                     class="w-64 h-[2px] bg-white"></span>
             </div>
 
-            <div class="text-center mb-7">
+            <div class="text-center  mb-7">
                 <button type="button" class="text-white border border-white px-7 py-3 rounded-[20px] " id="btnPerlombaan">Perlombaan</button>
                 <button type="button" class="text-white border border-white px-7 py-3 rounded-[20px]" id="btnSertifikat">Sertifikat</button>
             </div>
@@ -211,8 +211,8 @@
 
             <div class="flex flex-col px-5 gap-5 items-center justify-center relative" id="contentPerlombaan">
 
-                <p class="text-dark p-5 absolute text-[60px] rotate-12 left-36 z-30 top-[-80px] "><i class="fa-solid fa-trophy"></i></p>
-                <img src="{{ asset('image/bg-tp.png') }}" class="absolute top-[-220px] left-0" alt="">
+                <p class="text-dark p-5 absolute text-[60px]  rotate-12 left-0 md:left-36 z-30 top-[-80px] "><i class="fa-solid fa-trophy"></i></p>
+                <img src="{{ asset('image/bg-tp.png') }}" class="absolute top-[-220px] left-[-100px] mdleft-0" alt="">
 
                 <p class="text-dark p-5 absolute text-[60px] rotate-12 right-36 z-30 bottom-0 "><i class="fa-solid fa-award"></i></p>
                 <img src="{{ asset('image/bg-tp.png') }}" class="absolute bottom-[-180px] right-0" alt="">
@@ -328,7 +328,12 @@
                 </div>
             </div>
 
-            <div id="contentSertifikat">
+            <div id="contentSertifikat" class="relative">
+                <p class="text-dark p-5 absolute text-[60px]  rotate-12 left-0 md:left-36 z-30 top-[-80px] "><i class="fa-solid fa-trophy"></i></p>
+                <img src="{{ asset('image/bg-tp.png') }}" class="absolute top-[-220px] left-[-100px] mdleft-0" alt="">
+
+                <p class="text-dark p-5 absolute text-[60px] rotate-12 right-36 z-30 bottom-0 "><i class="fa-solid fa-award"></i></p>
+                <img src="{{ asset('image/bg-tp.png') }}" class="absolute bottom-[-180px] right-0" alt="">
 
             <div class="flex flex-col md:flex-row px-5 gap-8 items-center justify-center" >
                 <!-- Card Content -->
@@ -617,36 +622,7 @@
 
     @include('components.footer')
 
-    <!-- Main modal -->
-    <div id="crud-modal" tabindex="-1" aria-hidden="true"
-        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative p-4 w-full max-h-full">
-            <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-                <!-- Modal header -->
-                <div
-                    class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                        Resume
-                    </h3>
-                    <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-toggle="crud-modal">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                </div>
-                <!-- Modal body -->
-                <div class="p-4 md:p-5">
-                    <embed src="{{ asset('image/resume.pdf') }}" type="application/pdf" class="w-full h-[80vh]" />
-                </div>
-            </div>
-        </div>
-    </div>
+ 
 
 @push('scripts')
 <script>
